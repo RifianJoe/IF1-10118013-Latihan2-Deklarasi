15eb873fec5edfe59d5c075aa1a5d4ff2c37b5dc
RifianJoe <55788355+RifianJoe@users.noreply.github.com>
2019-09-26 15:03:05 +0700

    Add files via upload

---
+++ IF1-10118013-Latihan2-Deklarasi/src/if1/pkg10118013/latihan2/deklarasi/IF110118013Latihan2Deklarasi.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10118013.latihan2.deklarasi;
+
+/**
+ *
+ * @author Rifian
+ * Nama : Rifian Joe Premilenio
+ * Kelas : IF-1
+ * NIM : 10118013
+ * Deskripsi Program : Program ini berisi program untuk menampilkan jenis-jenis tipe data bilangan bulat
+ */
+public class IF110118013Latihan2Deklarasi {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // Deklarasi variabel
+        int nilaiInt;
+        final double PHI = 3.14; //konstanta uppercase
+        boolean nilaiLogika;
+        char nilaiKarakter;
+        
+        // Memberi nilai ke variabel
+        nilaiInt = 78;
+        nilaiLogika = false;
+        nilaiKarakter = 'D';
+        
+        // Menampilkan hasil
+        System.out.println();
+        System.out.println("Isi Variabel Nilai = " + nilaiInt);
+        System.out.println("Isi Variabel PHI = " + PHI);
+        System.out.println("Isi Variabel Logika = " + nilaiLogika);
+        System.out.println("Isi Variabel Karater = " + nilaiKarakter);
+    }
+    
+}
